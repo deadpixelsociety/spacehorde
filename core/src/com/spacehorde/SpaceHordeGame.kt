e510a4d3917b9bd2889d73d07fa2e56ea5a9aadc
Matt Sams <matt@thedeadpixelsociety.com>
2019-05-24 18:36:34 -0400

    Initial commit

---
+++ core/src/com/spacehorde/SpaceHordeGame.kt
@@ -0,0 +1,14 @@
+package com.spacehorde
+
+import com.badlogic.gdx.ApplicationAdapter
+
+class SpaceHordeGame : ApplicationAdapter() {
+    override fun create() {
+    }
+
+    override fun render() {
+    }
+
+    override fun dispose() {
+    }
+}
---
+++ desktop/src/com/spacehorde/desktop/DesktopLauncher.kt
@@ -0,0 +1,14 @@
+package com.spacehorde.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.spacehorde.SpaceHordeGame
+
+fun main(args: Array<String>) {
+    LwjglApplication(SpaceHordeGame(), LwjglApplicationConfiguration().apply {
+        title = "SpaceHorde"
+        width = 1280
+        height = 720
+        useGL30 = true
+    })
+}
